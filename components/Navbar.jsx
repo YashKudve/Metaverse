e9941e78aa47cb8d1237382ff7a5cf660ee3d62d
Yash <kudveyash@gmail.com>
2024-01-31 17:27:18 +0530

    Navbar design

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,14 @@ import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav variants={navVariants} initial="hidden" whileInView="show" className={`${styles.xPaddings} py-8 relative`}>
-    <div className='absolute w-[50%] inset-0'></div>
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img src="/search.svg" alt="Search" className='w-[24px] h-[24px] object-contain' />
+
+      <h2 className='font-extrabold text-[24px] leading-[30px] text-white uppercase'>Metaversus</h2>
+
+      <img src="/menu.svg" alt="menu" />
+    </div>
 
   </motion.nav>
 );
