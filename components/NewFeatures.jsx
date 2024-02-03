6602293ec4c78504ccf23974f6a07c836e83ff66
Yash <kudveyash@gmail.com>
2024-02-03 16:57:25 +0530

    new-feature section styling

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,5 +1,7 @@
-const NewFeatures = () => (
-  <div>
+import styles from "../styles";
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
     New Features
   </div>
 );
